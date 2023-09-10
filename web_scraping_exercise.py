0b28bd8a756dac4658be0bae7699115a7c3dde50
Charles Chudley <charliechudley@gmail.com>
2023-09-10 20:53:23 +0100

    Exercise complete

--- web_scraping_exercise.py
+++ web_scraping_exercise.py
@@ -1,35 +1,41 @@
 import requests
 import bs4
 
-
-# extract the titles of all 
-base_url = "http://books.toscrape.com/catalogue/page-{}.html"
-
-# page_num = 1 # Loop 1-50
-
-
-# res = requests.get(base_url.format(page_num))
+# base_url = "http://quotes.toscrape.com/"
+# res = requests.get(base_url)
 # soup = bs4.BeautifulSoup(res.text,'lxml')
 
-# products = soup.select(".product_pod")
+# authors_set = set()
+# author_class = soup.select(".author")
+# for item in author_class:
+#     authors_set.add(item.text)
 
-# example = products[0]
+# quotes_list = list()
+# quotes_class = soup.select(".text")
+# for item in quotes_class:
+#     quotes_list.append(item.text)
 
-# example.select(".star-rating.Two") ==[]
+# tags_list = list()
+# tags_class = soup.select(".tag-item")
+# for item in tags_class:
+#     tags_list.append(item.text)
 
-# example.select("a")[1]
 
-# book_title = example.select("a")[1]["title"]
+base_url = "http://quotes.toscrape.com/page/{}"
+page_num = 1
+res = requests.get(base_url.format(page_num))
+soup = bs4.BeautifulSoup(res.text,'lxml')
+authors_set = set()
 
-two_star_titles = []
-for page_num in range(1,51):
+while "No quotes found" not in soup.select(".col-md-8")[1].text:
+    author_class = soup.select(".author")
+    for item in author_class:
+        authors_set.add(item.text)
+
+    page_num += 1
     res = requests.get(base_url.format(page_num))
     soup = bs4.BeautifulSoup(res.text,'lxml')
-    products = soup.select(".product_pod")
-    for product in products:
-        if product.select(".star-rating.Two") != []:
-            two_star_titles.append(product.select("a")[1]["title"])
 
 
 
-print(two_star_titles)
+print("test")
